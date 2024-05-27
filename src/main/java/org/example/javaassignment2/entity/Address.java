5c2c82eac959ff17c05821169416d3d871b67a32
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-05-27 17:17:19 +0700

    feat: add method in address class

--- src/main/java/org/example/javaassignment2/entity/Address.java
+++ src/main/java/org/example/javaassignment2/entity/Address.java
@@ -28,4 +28,70 @@ public class Address {
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
+    public Address() {}
+
+    public Address(String city, String country, String state, String street, String zipCode, User user) {
+        this.city = city;
+        this.country = country;
+        this.state = state;
+        this.street = street;
+        this.zipCode = zipCode;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
