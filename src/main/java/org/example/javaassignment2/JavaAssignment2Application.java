5a9ef9f958caa0b06110ac34eddd8cb88bca8d1f
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-05-27 16:43:44 +0700

    setup project

---
+++ src/main/java/org/example/javaassignment2/JavaAssignment2Application.java
@@ -0,0 +1,13 @@
+package org.example.javaassignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAssignment2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAssignment2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javaassignment2/JavaAssignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaassignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAssignment2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
